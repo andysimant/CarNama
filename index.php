cc46c00805af73b644bfb235200f903855f96b40
Simant <Simant@MyDell>
2018-07-11 08:49:04 +0530

    First Save

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        
+        <link rel="stylesheet" type="text/css" href="Styles.css">
+        <meta charset="UTF-8">
+        <title>Car Nama</title>
+    </head>
+    
+    <body>
+        
+        <h2>Welcome to</h2>
+        <h1>CarNama.com</h1>
+        <div name="ad" bg-color="red" align = "center" background ="pink">
+            hello this is a div 
+        </div>    
+   
+        
+        <?php
+        // put your code here
+        echo"Hello";
+        ?>
+    </body>
+</html>
